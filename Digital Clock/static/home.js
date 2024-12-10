865d1c5f1785c0bac1c368657edbec7947f08fe8
Vaibhav <155850471+xdvae@users.noreply.github.com>
2024-12-10 23:42:15 +0530

    Update home.js
    Added new functions, Shows current time zone. And the search bar works

--- Digital Clock/static/home.js
+++ Digital Clock/static/home.js
@@ -1,16 +1,25 @@
+let Run_Default_Time = 1;
+let Run_City_Time = 0;
+let Current_city = 0;
 function Get_time(){
-    fetch('/gettime') 
-    .then(response => response.json()) 
-    .then(data => { 
-        console.log(data.hour);
-        document.getElementById('hour').innerText = data.hour + ':';
-        document.getElementById('min').innerText = data.min + ':';
-        document.getElementById('sec').innerText = data.sec;
-        document.getElementById('am-pm-text').innerText = data.am_pm;
+    
+    if(Run_Default_Time == 1){
+        Run_City_Time = 0;
+        fetch('/gettime') 
+        .then(response => response.json()) 
+        .then(data => { 
+            console.log(data.hour);
+            document.getElementById('hour').innerText = data.hour + ':';
+            document.getElementById('min').innerText = data.min + ':';
+            document.getElementById('sec').innerText = data.sec;
+            document.getElementById('am-pm-text').innerText = data.am_pm;
 
-     });
-    setTimeout(Get_time, 1000);
+        });
+        setTimeout(Get_time, 1000);
+
+    }
 }
+
 let dark_mode = 0;
 
 function set_Dark_Mode(){
@@ -40,5 +49,37 @@ function change_btn_class(){
         btn.classList.remove('Dark_mode_btn_2');
         btn.classList.add('Dark_mode_btn_1');
     }
+}
+// TODO: Add getlocation to show time in user's current location when they enter the site.
+
+function Get_city_time(){
+    Run_City_Time = 1;
+    if(Run_City_Time == 1){
+        Run_Default_Time = 0;
+    
+        const city_name = Current_city;
+        fetch('/get_city_time', {
+            method: 'POST',
+            headers: 
+            { 'Content-Type': 'application/json' },
+        body: JSON.stringify({
+            City_name: city_name
+        })}
+    )
+        .then(response => response.json()) 
+        .then(data => { 
+            console.log(data.hour);
+            document.getElementById('hour').innerText = data.hour + ':';
+            document.getElementById('min').innerText = data.min + ':';
+            document.getElementById('sec').innerText = data.sec;
+            document.getElementById('am-pm-text').innerText = data.am_pm;
+            document.getElementById('current-country-name').innerHTML = "Current Timezone: " + data.current_timezone
+        });
+        setTimeout(Get_city_time, 1000);
+    }
+}
+function update_city_value(){
+    Current_city = document.getElementById('search-bar-input').value;
+    Get_city_time();
 }
 window.onload = Get_time;
